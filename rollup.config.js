398a841c1a0be03f4a5008a85dc8237f1251858c
Blake Knight <oss.ideas@gmail.com>
2020-06-07 21:45:22 -0500

    :zap: greatly optimize build output

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
-import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
+const entry = './src/index.ts';
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-const name = /[a-z\-]+$/i.exec(pkg.name)[0];
+const externals = [...Object.keys(pkg.dependencies)];
+const name = /[a-z-]+$/i.exec(pkg.name)[0];
 
-export default {
-  input: './src/index.ts',
+const buildSystemConfig = {
+  external: externals,
+
+  input: entry,
 
   output: [
     {
@@ -18,32 +23,8 @@ export default {
     {
       file: pkg.module,
       format: 'esm'
-    },
-    {
-      file: pkg.browser,
-      format: 'esm',
-      plugins: [
-        getBabelOutputPlugin({
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                bugfixes: true,
-                forceAllTransforms: true,
-                loose: true,
-                modules: 'umd',
-                targets: '> 0.25%, not dead'
-              }
-            ]
-          ],
-          // Sets the global name used for the UMD export
-          filename: name
-        })
-      ]
     }
   ],
-
-  external: [],
 
   plugins: [
     // Allows node_modules resolution
@@ -56,3 +37,51 @@ export default {
     babel({ babelHelpers: 'bundled', extensions, include: ['src/**/*'] })
   ]
 };
+
+const browserConfig = {
+  input: entry,
+
+  output: {
+    exports: 'named',
+    file: pkg.browser,
+    format: 'umd',
+    globals: externals.reduce((globals, name) => {
+      globals[name] = name;
+      return globals;
+    }),
+    name,
+    sourcemap: true
+  },
+
+  plugins: [
+    resolve({
+      mainFields: ['module', 'browser', 'main']
+    }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+
+    // Compile TypeScript/JavaScript files
+    babel({
+      babelHelpers: 'runtime',
+      extensions,
+      plugins: ['@babel/plugin-transform-runtime'],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            bugfixes: true,
+            forceAllTransforms: true,
+            loose: true,
+            modules: false,
+            targets: '> 0.25%, not dead'
+          }
+        ]
+      ]
+    }),
+
+    terser()
+  ]
+};
+
+export default [buildSystemConfig, browserConfig];
