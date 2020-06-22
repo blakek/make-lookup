7f0886c8951f70d068495384fe944f0b19b4659c
Blake Knight <bknight@gsandf.com>
2020-06-22 09:31:09 -0500

    :green_heart: use generic type instead of `any`
    resolves #1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { get, Path } from '@blakek/deep';
 
-export function makeLookup<T>(
-  inputArray: any[],
+export function makeLookup<T extends Record<string, any>>(
+  inputArray: T[],
   lookupProperty: Path
 ): Record<string, T> {
   return inputArray.reduce(
-    (lookup: Record<string, T>, next: Record<string, any>) => ({
+    (lookup: Record<string, T>, next: T) => ({
       ...lookup,
       [get(lookupProperty, next)]: next
     }),
